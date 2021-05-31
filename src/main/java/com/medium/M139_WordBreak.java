225a6bb7c3d1f31746b5d9e45a24ca0399f75859
xujunkang <xujunkangchn@gmail.com>
2021-05-31 23:51:14 +0800

    139 背包问题

--- src/main/java/com/medium/M139_WordBreak.java
+++ src/main/java/com/medium/M139_WordBreak.java
@@ -2,15 +2,41 @@ package com.medium;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author icodeboy
  */
 public class M139_WordBreak {
+
+    /**
+     * 动态规划实现
+     * dp[i]定义：表示以i结尾的字符串是否可以被wordDict中组合而成
+     * base case: dp[0]=true，表示空串且合法
+     * 状态转移方程：dp[i] = dp[i] || dp[i-wordLength]
+     *
+     *  主要发现主问题和子问题的关系：主问题的解一定包括了子问题的解
+     *  这道题里面，如果长串可以被分解，那么从长串最右边减去一个单词的子串，也一定可以被分解；
+     *  所以对于一个当前串，每次从末尾减去单词表里面的单词，再看看子串能不能分解。
+     *  若一个子串可以分解，再加上删去的单词，则源串一定可以分解。
+     */
+    public boolean wordBreak (String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        Arrays.fill(dp, false);
+        dp[0] = true;
+        for (int i = 1; i < s.length() + 1; i++) {
+            for (int j = 0; j < wordDict.size(); j++) {
+                String word = wordDict.get(j);
+                int wl = word.length();
+                // dp[i - wl] 表示子串
+                if(i - wl >= 0 && dp[i - wl] && Objects.equals(s.substring(i - wl, i), word)){
+                    dp[i] = true;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
     /**
      * DFS 实现
      *  1. 逐位遍历单词，看看在单词表示能否匹配上
@@ -22,7 +48,7 @@ public class M139_WordBreak {
      */
     private boolean canBreak = false;
     private Map<Integer, Boolean> map;
-    public boolean wordBreak(String s, List<String> wordDict) {
+    public boolean wordBreakByDFS(String s, List<String> wordDict) {
         map = new HashMap<>();
         return dfs(s, wordDict, 0);
     }
